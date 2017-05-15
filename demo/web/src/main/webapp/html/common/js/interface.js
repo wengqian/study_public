335857c3437a5c8a5424a50dad021724f7416cd5
wengqian <18767126010@sina.cn>
2017-05-15 11:15:36 +0800

    wq

--- demo/web/src/main/webapp/html/common/js/interface.js
+++ demo/web/src/main/webapp/html/common/js/interface.js
@@ -14,6 +14,7 @@ var ProjectController ={
     opeartion_project_data:visit_url+'/projectController/opeartion_project_data.do',//项目
     opeartion_project_vist_power:visit_url+'/projectController/opeartion_project_vist_power.do',//访问人员
     opeartion_project_ac_board:visit_url+'/projectController/opeartion_project_ac_board.do',//留言板
+    multiple_message_board_exportWord:visit_url+'/projectController/multiple_message_board_exportWord.do',//导出,id
     opeartion_risk_control:visit_url+'/projectController/opeartion_risk_control.do',//发送短信
     opeartion_project_multiple_message_board:visit_url+'/projectController/opeartion_project_multiple_message_board.do',//多项留言板
     opeartion_sys_user:visit_url+'/projectController/opeartion_sys_user.do'//操作用户
